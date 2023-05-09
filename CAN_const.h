c0cdafce135faa5b7273628679cdec726dae46ce
CANguru-System <gustavwostrack@web.de>
2023-05-09 09:51:52 +0200

    First commit

--- CAN_const.h
+++ CAN_const.h
@@ -6,5 +6,6 @@ const uint8_t CAN_FRAME_SIZE = 13; /* maximum datagram size */
 #define CAN_MSG_FLAG_EXTD_ 0b01
 #define CAN_MSG_FLAG_SS_ 0b00
 #define CAN_MSG_FLAG_RTR_ 0b00
+#define CAN_EFF_MASK 0x1FFFFFFFU /* extended frame format (EFF) */
 
 #endif 
